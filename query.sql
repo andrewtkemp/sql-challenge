e73ae86c4598c9bddc1363cb81595511900d49ca
kehull <71107280+kehull@users.noreply.github.com>
2021-01-12 19:46:44 -0600

    csvs all imported and required scripts created

--- query.sql
+++ query.sql
@@ -40,7 +40,63 @@ CREATE TABLE DEPT_MANAGER (
 CREATE TABLE DEPT_EMPLOYEE (
 	emp_no INT NOT NULL,
 	dept_no VARCHAR NOT NULL,
-	PRIMARY KEY(emp_no),
+	PRIMARY KEY(emp_no, dept_no),
 	FOREIGN KEY(emp_no) REFERENCES EMPLOYEES(emp_no),
 	FOREIGN KEY(dept_no) REFERENCES DEPARTMENTS(department_no)
 );
+
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM SALARIES as s
+LEFT JOIN EMPLOYEES as e
+ON e.emp_no = s.emp_no;
+
+--List first name, last name, and hire date for employees who were hired in 1986.
+
+SELECT first_name, last_name, hire_date FROM EMPLOYEES
+WHERE hire_date LIKE '%/1986';
+
+--List the manager of each department with the following information: 
+	--department number, department name, the manager's employee number, last name, first name
+SELECT d.department_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM DEPT_MANAGER as dm
+LEFT JOIN DEPARTMENTS as d ON dm.dept_no = d.department_no
+LEFT JOIN EMPLOYEES as e ON dm.emp_no = e.emp_no;
+
+--List the department of each employee with the following information: 
+	--employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM DEPT_EMPLOYEE as de
+	LEFT JOIN EMPLOYEES as e 
+		ON de.emp_no = e.emp_no
+	LEFT JOIN DEPARTMENTS as d
+		ON de.dept_no = d.department_no;
+
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B.
+SELECT first_name, last_name, sex FROM EMPLOYEES 
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+--List all employees in the Sales department, including their 
+	--employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM DEPT_EMPLOYEE as de
+	INNER JOIN EMPLOYEES as e
+		ON de.emp_no = e.emp_no
+	INNER JOIN DEPARTMENTS as d
+		ON de.dept_no = d.department_no
+		WHERE d.dept_name = 'Sales';
+
+--List all employees in the Sales and Development departments, including their 
+	--employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM DEPT_EMPLOYEE as de
+	INNER JOIN EMPLOYEES as e
+		ON de.emp_no = e.emp_no
+	INNER JOIN DEPARTMENTS as d
+		ON de.dept_no = d.department_no
+		WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development';
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(last_name) AS ln_total
+FROM EMPLOYEES GROUP BY last_name ORDER BY ln_total;
